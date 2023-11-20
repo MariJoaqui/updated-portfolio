cbf6fb34dea03eb27aee0ebfe86d1039939281e1
Maria Joaquina <morenoaulaciomariajoaquina@gmail.com>
2023-11-20 17:53:02 -0400

    Inicios en experiencias

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 
 // Material
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule
