1bf0ce3c7120dd772c12aa42589f65456dfcaeff
Maria Joaquina <morenoaulaciomariajoaquina@gmail.com>
2023-11-29 10:03:25 -0400

    Habilidades listas

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -30,19 +30,19 @@ export class HomeComponent implements OnInit {
       this.image = '../../../assets/img/skills.png';
       this.information = 'Habilidades';
       this.description = 1;
-      this.shortDescription = 'una frase aqui, la primera, algo muy corto porfavor, no se que aun pero aja. aaaaaaaaaaaaaaaaaaaaaaaaaaa fd f rgt tgr yty hty hj yu j it kj uik  lk  l p lo  ku hb t thgythbthb yhju juy j iu k ku km  hy yt ht nh.';
+      this.shortDescription = 'Me gusta plasmar mis ideas visualmente antes de llevarlas al código. Disfruto creando diseños previos en programas especializados, asegurando un enfoque estético y funcional en cada proyecto.';
     }
     else if ( info === 2 ) {
       this.image = '../../../assets/img/interests.png';
       this.information = 'Intereses';
       this.description = 2;
-      this.shortDescription = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat minima, corrupti voluptas sit similique auterror quae ea suscipit laborum atque quo vero perspiciatis ut voluptates perferendis minus odit nihil.';
+      this.shortDescription = '';
     }
     else if ( info === 3 ) {
-      this.image = '../../../assets/img/skills.png';
+      this.image = '../../../assets/img/others.png';
       this.information = 'Otros';
       this.description = 3;
-      this.shortDescription = 'una frase aqui, la primera ddddddddds sssssssssssssssr kjibbufd gfbgfhbhbhnnh u hb t thgythbthb yhju juy j iu k ku km sdshfuieraf ehreurhfgfbv ytutjuujy hy yt ht nhhhhhhhhhhhhhhghghytyjjynuimhhhhhhhhhhhhhhhhhhhhh.';
+      this.shortDescription = 'Estas actividades me permiten expresar mi creatividad y me ayudan a mantener un equilibrio en mi vida.';
     }
   }
 
