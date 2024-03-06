eaad94bef78a9ddd7a515ebcf8c9747a67098216
Maria Joaquina <84864759+MariJoaqui@users.noreply.github.com>
2024-03-06 11:45:40 -0400

    Arreglo en el código del app-component

--- src/app/shared/interfaces/interfaces.ts
+++ src/app/shared/interfaces/interfaces.ts
@@ -1,11 +1,11 @@
 export interface Cards {
-  title       : string,
-  description : string,
-  image       : string,
-  time        : string
+  title: string,
+  description: string,
+  image: string,
+  time: string
 }
 export interface Links {
-  name   : string,
-  link   : string | number,
-  scroll : string,
+  name: string,
+  link: string | number,
+  scroll: string,
 }
